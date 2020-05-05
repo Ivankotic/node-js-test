3eaa793afa25e948d165040ab086fad7be4411d4
Ivankotic <arduino272727@gmail.com>
2020-05-05 14:18:35 +0500

    first commit24234

--- src/js/logic.js
+++ src/js/logic.js
@@ -2,6 +2,8 @@ var socket = io();
 var messages = document.getElementById("messages");
 var message_text = document.getElementById("message_text");
 var send_button = document.getElementById("send_button");
+const port = process.env.PORT || 80;
+
 
 socket.on('show_messages', function (data) {
     messages.innerHTML = data.messages;
