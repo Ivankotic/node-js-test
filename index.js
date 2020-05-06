959915c7934ff372eebdbeb88a2108e5db55a50d
Ivankotic <arduino272727@gmail.com>
2020-05-06 14:14:53 +0500

    mongo db

--- index.js
+++ index.js
@@ -1,46 +1,73 @@
 let app = require('express')();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
-let sockets = [];
-let text = '';
+const MongoClient = require('mongodb').MongoClient;
 
 let port = process.env.PORT;
+let sockets = [];
+const uri = "mongodb+srv://db_user:esesrere123@cluster0-i4qnu.gcp.mongodb.net/test?retryWrites=true&w=majority";
+let db;
+let db_ok = false;
 
-app.get('/', function(req, res){
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(function (err) {
+    db = client.db('test_db');
+    db_ok = true;
+});
+
+function update_text(func) {
+    if (db_ok) {
+        db.collection('test_col').find({}).toArray(function (err, result) {
+            let text = result[0].db_text;
+            func(text);
+        });
+    }
+}
+
+app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
- 
-app.get('/css', function(req, res){
+
+app.get('/css', function (req, res) {
     res.sendFile(__dirname + '/src/css/style.css');
 });
 
-app.get('/logic', function(req, res){
+app.get('/logic', function (req, res) {
     res.sendFile(__dirname + '/src/js/logic.js');
 });
 
 if (port == null || port == "") {
-  port = 80;
+    port = 80;
 }
 
 http.listen(port);
 
-function show() {
-    sockets.forEach(function(thisSocket) {
+function show(text) {
+    sockets.forEach(function (thisSocket) {
         thisSocket.emit('show_messages', { messages: text });
     });
 }
 
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
     sockets.push(socket);
 
-    show();
+    update_text(function (text) {
+        show(text);
 
-    socket.on('new_message', function (data) {
-        text += data.message;
-        show();
+        socket.on('new_message', function (data) {
+            update_text(function (the_text) {
+                var myquery = { db_text: the_text };
+                var newvalues = { $set: { db_text: the_text + data.message } };
+                db.collection('test_col').updateOne(myquery, newvalues, function () {
+                    update_text(function (new_text) {
+                        show(new_text);
+                    });
+                });
+            });
+        });
     });
 });
 
-setInterval(function() {
+setInterval(function () {
     sockets = [];
-}, 1000 * 60 * 60 * 1);
+}, 1000 * 60 * 30 * 1);
