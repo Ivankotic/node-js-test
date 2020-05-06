8825acb91fb08304622618ca0a25182073e24b75
Ivankotic <arduino272727@gmail.com>
2020-05-06 20:46:21 +0500

    chat test

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ client.connect(function (err) {
     db_ok = true;
 });
 
-function update_text(func) {
+function get_text(func) {
     if (db_ok) {
         db.collection('test_col').find({}).toArray(function (err, result) {
             let text = result[0].db_text;
@@ -51,15 +51,18 @@ function show(text) {
 io.on('connection', function (socket) {
     sockets.push(socket);
 
-    update_text(function (text) {
+    get_text(function (text) {
         show(text);
 
         socket.on('new_message', function (data) {
-            update_text(function (the_text) {
-                var myquery = { db_text: the_text };
-                var newvalues = { $set: { db_text: the_text + data.message } };
+            get_text(function (the_text) {
+                let date = new Date();
+                let new_message_html = '<div class="message"><p class="date">' + date.getDate() + '.' + (date.getMonth() + 1) + '.' + date.getFullYear() +'</p>' + '<p class="time">' + date.getHours() + ':' + date.getMinutes() + '</p><p class="text">' + data.message + '</p></div>';
+
+                let myquery = { db_text: the_text };
+                let newvalues = { $set: { db_text: the_text + new_message_html} };
                 db.collection('test_col').updateOne(myquery, newvalues, function () {
-                    update_text(function (new_text) {
+                    get_text(function (new_text) {
                         show(new_text);
                     });
                 });
