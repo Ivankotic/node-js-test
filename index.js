e8100cfa12f7ee8dc93792c787350fbfcb7d4885
Ivankotic <arduino272727@gmail.com>
2020-05-07 22:30:29 +0500

    chat123

--- index.js
+++ index.js
@@ -10,67 +10,67 @@ let db;
 let db_ok = false;
 
 const client = new MongoClient(uri, { useNewUrlParser: true });
+
 client.connect(function (err) {
     db = client.db('test_db');
-    db_ok = true;
-});
 
-function get_text(func) {
-    if (db_ok) {
+    function get_text(func) {
         db.collection('test_col').find({}).toArray(function (err, result) {
-            let text = result[0].db_text;
+            let text = ''; 
+            for (let i = 0; i < result.length; i++) {
+                text = text + result[i].html;
+            }
             func(text);
+            console.log(result);
+        });
+
+    }
+
+    function show(text) {
+        sockets.forEach(function (thisSocket) {
+            thisSocket.emit('show_messages', { messages: text });
         });
     }
-}
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
-
-app.get('/css', function (req, res) {
-    res.sendFile(__dirname + '/src/css/style.css');
-});
-
-app.get('/logic', function (req, res) {
-    res.sendFile(__dirname + '/src/js/logic.js');
-});
-
-if (port == null || port == "") {
-    port = 80;
-}
-
-http.listen(port);
-
-function show(text) {
-    sockets.forEach(function (thisSocket) {
-        thisSocket.emit('show_messages', { messages: text });
+    app.get('/', function (req, res) {
+        res.sendFile(__dirname + '/index.html');
     });
-}
 
-io.on('connection', function (socket) {
-    sockets.push(socket);
+    app.get('/css', function (req, res) {
+        res.sendFile(__dirname + '/src/css/style.css');
+    });
 
-    get_text(function (text) {
-        show(text);
+    app.get('/logic', function (req, res) {
+        res.sendFile(__dirname + '/src/js/logic.js');
+    });
 
-        socket.on('new_message', function (data) {
-            get_text(function (the_text) {
+    if (port == null || port == "") {
+        port = 80;
+    }
+
+    http.listen(port);
+
+    io.on('connection', function (socket) {
+        sockets.push(socket);
+
+        get_text(function (text) {
+            show(text);
+
+            socket.on('new_message', function (data) {
                 let date = new Date();
-                let new_message_html = '<div class="message"><p class="date">' + date.getDate() + '.' + (date.getMonth() + 1) + '.' + date.getFullYear() +'</p>' + '<p class="time">' + date.getHours() + ':' + date.getMinutes() + '</p><p class="text">' + data.message + '</p></div>';
+                let new_message_html = '<div class="message"><p class="date">' + date.getDate() + '.' + (date.getMonth() + 1) + '.' + date.getFullYear() + '</p>' + '<p class="time">' + date.getHours() + ':' + date.getMinutes() + '</p><p class="text">' + data.message + '</p></div>';
 
-                let myquery = { db_text: the_text };
-                let newvalues = { $set: { db_text: new_message_html + the_text} };
-                db.collection('test_col').updateOne(myquery, newvalues, function () {
-                    get_text(function (new_text) {
-                        show(new_text);
-                    });
+                let message = { html: new_message_html };
+                db.collection('test_col').insert(message);
+                get_text(function (new_text) {
+                    show(new_text);
                 });
             });
         });
     });
-});
 
-setInterval(function () {
-    sockets = [];
-}, 1000 * 60 * 30 * 1);
+    setInterval(function () {
+        sockets = [];
+    }, 1000 * 60 * 30 * 1);
+
+});
